ae0a86a961077782dac65de3f9d23f4aac6367f3
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-04-29 20:53:04 +0600

    hour-to-min

---
+++ hour-to-min.js
@@ -0,0 +1,8 @@
+function hoursToMin(hours) {
+  console.log("The provided hours are : ", hours);
+  mins = hours * 60;
+  return mins;
+}
+
+let x = hoursToMin(24);
+console.log("The provided hours refers to ", x, "minutes.");
